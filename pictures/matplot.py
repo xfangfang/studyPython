a2db0936de346cb2b17a806cbcc445f3819ad4f1
xfang <2553041586@qq.com>
2016-09-09 23:07:05 +0800

    绘制函数图像

--- pictures/matplot.py
+++ pictures/matplot.py
@@ -1,5 +1,24 @@
-import numpy as np
-import pylab as pl
-import matplotlib.font_manager as fm
+#!/usr/bin/env python
+# coding=utf-8
 
- 
+
+import numpy as np
+from matplotlib import pylab, mlab, pyplot
+import scipy
+import matplotlib.font_manager as fm
+import time
+
+x = np.arange(-8, 8, 0.1);
+y = np.sin(x)
+pyplot.figure(figsize=(8,4))
+pyplot.xlabel('x')
+pyplot.ylabel('y')
+pyplot.title('sin 函数'.decode('utf-8'))
+pyplot.plot(x, y,'r.')
+pyplot.plot(x,y,'g')
+pyplot.ylim(-2, 2)
+pyplot.xlim(-8, 8)
+f = pyplot.gcf()
+q = pyplot.gca()
+print f,q
+# pyplot.show()
