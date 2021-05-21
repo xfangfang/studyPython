b6f21441cb8400dd6e049770a27c69bea8df5bc6
xfangfang <2553041586@qq.com>
2021-05-22 02:28:50 +0800

    Update and rename we_sing.1d.py to ksing.1d.py

--- ksing.1d.py
+++ ksing.1d.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env PYTHONIOENCODING=UTF-8 /usr/local/bin/python3
 
 # xbar Metadata
-# <xbar.title>WeSing 全民K歌</xbar.title>
-# <xbar.version>v1.0</xbar.version>
-# <xbar.author>Fang Yuecheng</xbar.author>
+# <xbar.title>KSing 全民K歌</xbar.title>
+# <xbar.version>v1.1</xbar.version>
+# <xbar.author>xfangfang</xbar.author>
 # <xbar.author.github>xfangfang</xbar.author.github>
-# <xbar.desc>Play songs from WeSing</xbar.desc>
+# <xbar.desc>Play songs from KSing</xbar.desc>
 # <xbar.image>https://raw.githubusercontent.com/xfangfang/xfangfang.github.io/master/assets/img/we_sing.png</xbar.image>
-# <xbar.dependencies>python3,requests,requests_futures,playsound</xbar.dependencies>
-# <xbar.abouturl>https://xfangfang.github.io/python/019</xbar.abouturl>
-# <xbar.var>string(VAR_ID="64999a87232a308d"): User ID. (String of length 16) </xbar.var>
+# <xbar.dependencies>python3,requests,requests_futures,playsound,pyobjc</xbar.dependencies>
+# <xbar.abouturl>https://xfangfang.github.io/019</xbar.abouturl>
+# <xbar.var>select(VAR_ID="679b9982232b318b"): User ID. (String of length 16) [64999a87232a308d, 679b9982232b318b]</xbar.var>
 
 import re
 import os
@@ -25,12 +25,14 @@ from requests_futures.sessions import FuturesSession
 from concurrent.futures import as_completed
 
 USERID = os.getenv('VAR_ID')
+SCRIPT_NAME = sys.argv[0][2:] # sys.argv[0] = ./ksing.1d.py
 
 class jsonEnconding(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, Song):
             data = {
                 'shareid': o.shareid,
+                'ksong_mid': o.ksong_mid,
                 'time': o.time,
                 'title': o.title
             }
@@ -40,9 +42,9 @@ class jsonEnconding(json.JSONEncoder):
 class Object():
     session = requests.Session()
     future_session = FuturesSession(session=session)
-    CACHE_PATH = '{}/.we_sing_cache'.format(os.path.expanduser('~'))
+    CACHE_PATH = '{}/.ksing_cache'.format(os.path.expanduser('~'))
     SETTING_CACHE = '{}/setting.json'.format(CACHE_PATH)
-    NUM_PER_PAGE = 15
+    NUM_PER_PAGE = 8
     REQUEST_WORKERS = 4
     UPDATE_TIME = 86400 # 86400 = 1 day
 
@@ -65,7 +67,7 @@ class Object():
 
     def getText(self, url):
         return self.session.get(url, headers=self.headers).text
-    
+
     @staticmethod
     def timeConverter(unixTime):
         dateArray = datetime.datetime.utcfromtimestamp(unixTime)
@@ -77,14 +79,14 @@ class Object():
             os.makedirs(Player.CACHE_PATH)
 
 class Song(Object):
-    def __init__(self, shareid, time=0, title='', avatar='', play_url='', play_url_video=''):
-        self.avatar = avatar
+    def __init__(self, ksong_mid, time, shareid, title='', play_url='', play_url_video=''):
         self.title = title.replace('&#39;',"'")
         self.time = time
         self.shareid = shareid
+        self.ksong_mid = ksong_mid
         self.play_url = play_url
         self.play_url_video = play_url_video
-        
+
     def getPlayUrl(self):
         if self.play_url != '': return self.play_url
         if self.play_url_video != '': return self.play_url_video
@@ -92,7 +94,7 @@ class Song(Object):
         self.play_url = re.findall(r'playurl":"(.*?)",', content)[0]
         self.play_url_video = re.findall(r'playurl_video":"(.*?)","', content)[0]
         return self.play_url_video if self.play_url == '' else self.play_url
-    
+
     def getContent(self):
         return self.session.get(self.getPlayUrl()).content
 
@@ -101,9 +103,9 @@ class Song(Object):
 
     def __lt__(self, other):
             return self.time > other.time
-        
+
 class Player(Object):
-    
+
     def __init__(self, userid, total_num = 0, nick_name = '', age = 0, gender=2):
         self.total_num = total_num
         self.userid = userid
@@ -111,45 +113,44 @@ class Player(Object):
         self.age = age
         self.gender = gender
         self.playlist = []
-        
+
     def _getInfo(self, page):
         url = "https://node.kg.qq.com/cgi/fcgi-bin/kg_ugc_get_homepage?type=get_uinfo&" + \
             "start={}&num={}&share_uid={}&callback=MusicJsonCallback".format(page, self.NUM_PER_PAGE, self.userid)
         content = self.getText(url)
         data = re.findall(r'[(](.*)[)]', content)[0]
         return json.loads(data)['data']
-    
+
     def _getInfoAsync(self, page):
         url = "https://node.kg.qq.com/cgi/fcgi-bin/kg_ugc_get_homepage?type=get_uinfo&" + \
             "start={}&num={}&share_uid={}&callback=MusicJsonCallback".format(page, self.NUM_PER_PAGE, self.userid)
         return self.future_session.get(url, headers = self.headers)
-        
-    def _addDateToPlaylist(self, songs_data_list):
+
+    def _addDataToPlaylist(self, songs_data_list):
         for data in songs_data_list:
-            song = Song(data['shareid'], data['time'], data['title'], data['avatar'])
+            song = Song(data['ksong_mid'], data['time'], data['shareid'], data['title'])
             self.playlist.append(song)
 
     @staticmethod
     def createCacheDir():
         if not os.path.exists(Player.CACHE_PATH):
             os.makedirs(Player.CACHE_PATH)
-            
+
     def createSongCacheDir(self):
         Player.createCacheDir()
         song_cache_dir = '{}/{}'.format(self.CACHE_PATH, self.userid)
         if not os.path.exists(song_cache_dir):
             os.makedirs(song_cache_dir)
 
-    
     def save(self):
         self.createSongCacheDir()
         user_profile_path = "{}/user_{}.json".format(self.CACHE_PATH, self.userid)
         with open(user_profile_path,'w',encoding='utf-8') as f:
             json.dump(obj = self, fp = f, cls = jsonEnconding, ensure_ascii=False)
-            
+
     def saveSong(self, song):
         self.createSongCacheDir()
-        song_path = '{}/{}/{}.m4a'.format(Player.CACHE_PATH, self.userid, song.shareid)
+        song_path = '{}/{}/{}-{}.m4a'.format(Player.CACHE_PATH, self.userid, song.ksong_mid, song.time)
         if os.path.exists(song_path): return song_path
         with open(song_path, 'wb') as f:
             f.write(song.getContent())
@@ -164,10 +165,10 @@ class Player(Object):
             d = json.load(fp=f)
         p = Player(d['userid'], d['total_num'], d['nick_name'], d['age'], d['gender'])
         for i in d['playlist']:
-            p.playlist.append(Song(i['shareid'], i['time'], i['title']))
+            p.playlist.append(Song(i['ksong_mid'], i['time'], i['shareid'], i['title']))
         return p
 
-    
+
     def getPlaylist(self):
         self.playlist = []
         content = self._getInfo(1)
@@ -177,19 +178,19 @@ class Player(Object):
         self.gender = content['gender']
         if self.total_num == 0:
             return self.playlist
-        self._addDateToPlaylist(content['ugclist'])
+        self._addDataToPlaylist(content['ugclist'])
         total_page = math.ceil(self.total_num/self.NUM_PER_PAGE)
-        
+
         futures=[self._getInfoAsync(i) for i in range(2, total_page+1)]
         p = 1
         for future in as_completed(futures):
             p += 1
             content = future.result().text
             data = re.findall(r'[(](.*)[)]', content)[0]
-            self._addDateToPlaylist(json.loads(data)['data']['ugclist'])
+            self._addDataToPlaylist(json.loads(data)['data']['ugclist'])
         self.playlist.sort()
         return self.playlist
-    
+
     def __repr__(self):
         userinfo = "name:{} gender:{} age:{}\n".format(self.nick_name, self.gender, self.age)
         for index, song in enumerate(self.playlist):
@@ -203,11 +204,11 @@ class Player(Object):
 class Setting(Object):
     def __init__(self, loop='Ascend', current='', currentid='', current_userid='', showtime=False, shownotify=True, lastupdate=0):
         self.loop = loop
-        self.current = current
-        self.currentid = currentid
-        self.showtime = showtime
-        self.shownotify = shownotify
-        self.lastupdate = lastupdate
+        self.current = current # current song title
+        self.currentid = currentid # current song shareid
+        self.showtime = showtime # show songs' publish time
+        self.shownotify = shownotify # notify when play songs
+        self.lastupdate = lastupdate # last playlist update time
         self.current_userid = current_userid
 
     def save(self):
@@ -224,7 +225,7 @@ class Setting(Object):
              d['shownotify'], d['lastupdate'])
 
 def refreshPlugin():
-    os.system("open -jg \'xbar://app.xbarapp.com/refreshPlugin?path={}\'".format(sys.argv[0]))
+    os.system("open -jg \'xbar://app.xbarapp.com/refreshPlugin?path={}\'".format(SCRIPT_NAME))
 
 def killAll():
     def is_number(s):
@@ -236,7 +237,7 @@ def killAll():
         return False
 
     def getProcess():
-        cmd = r'ps -ef | grep we_sing.*.py'
+        cmd = r'ps -ef | grep ksing.*.py'
         res = os.popen(cmd).readlines()
         pids = []
         for line in res:
@@ -361,7 +362,7 @@ def loadPlayer(setting, forceUpdate = False):
         pass
     finally:
         if player == None:
-            player = updateFromInternet()    
+            player = updateFromInternet()
     return player
 
 def getDirSize(dir):
@@ -373,7 +374,7 @@ def getDirSize(dir):
 def checkUpdateTime(setting):
     lastupdate = setting.lastupdate
     seconds = int(round(time.time()))
-    if seconds - lastupdate > Setting.UPDATE_TIME :        
+    if seconds - lastupdate > Setting.UPDATE_TIME :
         player = loadPlayer(setting, True)
         if player != None:
             setting.lastupdate = seconds
@@ -381,7 +382,7 @@ def checkUpdateTime(setting):
             return player
     return None
 
-def cmd(title, p1, p2='nothing', refresh=False, color = 'balck'):
+def cmd(title, p1, p2='nothing', refresh=False, color='black'):
     current = int(round(time.time()))
     print("{} | shell='{}' param1={} param2={} param3={} terminal=false refresh={} color={}".format(title, sys.argv[0], p1, p2, current, refresh, color))
 
@@ -420,7 +421,7 @@ def main():
     if player is None:
         print("Wrong UserID")
         print("---")
-        print("Seting UserID | href=xbar://app.xbarapp.com/openPlugin?path={}".format(sys.argv[0]))
+        print("Seting UserID | href=xbar://app.xbarapp.com/openPlugin?path={}".format(SCRIPT_NAME))
         return
     else:
         text = setting.current if setting.current != '' else player.nick_name
@@ -434,7 +435,7 @@ def main():
         else:
             song_name = "{}: {}".format(i+1, song.title.replace("|", "/"))
         color = 'blue' if setting.currentid == song.shareid else 'black'
-        cmd("--{}".format(song_name), "play", i, color = color)
+        cmd("--{}".format(song_name), "play", i, refresh=True, color=color)
     cmd("Stop", "stop", refresh=True)
     print("Loop - {}".format(setting.loop))
     print("---")
@@ -451,8 +452,8 @@ def main():
     text = "Hide" if setting.showtime else "Show"
     cmd("--{} PublishTime".format(text), "time", text, refresh=True)
     cmd("--Clear Cache: {}".format(getDirSize(Object.CACHE_PATH)), 'clear', refresh=True)
-    print("--Seting UserID | href=xbar://app.xbarapp.com/openPlugin?path={}".format(sys.argv[0]))
-    print("--Help | href=https://xfangfang.github.io/python/019")
+    print("--Seting UserID | href=xbar://app.xbarapp.com/openPlugin?path={}".format(SCRIPT_NAME))
+    print("--Help | href=https://xfangfang.github.io/019")
 
 
 if __name__ == "__main__":
